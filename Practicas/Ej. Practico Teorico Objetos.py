f9e6e2564c0ad645e8b8cd8be9348fef22488e0d
mateofelgueras <mateofelgueras@gmail.com>
2022-04-18 20:28:26 -0300

    Update Ej. Practico Teorico Objetos.py

--- Practicas/Ej. Practico Teorico Objetos.py
+++ Practicas/Ej. Practico Teorico Objetos.py
@@ -1,4 +1,9 @@
-class Golondrina:
+#Clase AnimalesAlados que hereda a Golondrina y Dragon.
+class AnimalesAlados:
+  def esta_feliz(self):
+    return self.energia > 500
+
+class Golondrina(AnimalesAlados):
   def __init__(self, energia):
     self.energia = energia
 
@@ -10,15 +15,15 @@ class Golondrina:
 
   def volar(self, kms):
     self.energia -= 10 + kms
-
+#Funciones esta_debil y  esta_feliz agregadas.
   def esta_debil(self):
     return self.energia < 10
-  
-  def esta_feliz(self):
-    return self.energia > 500
 
+pepita=Golondrina(100)
+anastasia = Golondrina(200)
+maria = Golondrina(42)
 
-class Dragon:     
+class Dragon(AnimalesAlados):     
   def __init__(self, cantidad_dientes, energia):
     self.energia = energia
     self.cantidad_dientes = cantidad_dientes
@@ -34,21 +39,38 @@ class Dragon:
 
   def volar(self, kms):
     self.energia -= 10 + kms/10
-  
+#Funciones esta_debil agregada. 
   def esta_debil(self):
     return self.energia < 50
+
+#Creo clase Entrenador
+class Entrenador:
+  def _init_(self, equipo):
+    self.equipo = equipo
+
+  def el_equipo(self):
+    return self.equipo
+
+  def agregar_animal(self, dragon):
+    self.equipo.append(dragon)
+ 
+  def entrenar_dragon(self, dragon):
+    for i in range(20):
+      dragon.volar_en_circulos()
+    dragon.comer_peces(3)
+
+  def entrenar_equipo(self):
+    for dragon in self.equipo:
+      self.entrenar_dragon
   
-  def esta_feliz(self):
-    return self.energia > 500
+  def entrenamiento_intensivo(self):
+    for dragon in self.equipo:
+      while dragon.esta_debil == False:
+        dragon.volar_en_circulos()
+       
 
-  def hipo(self):
-    self.energia == self.volar_en_circulos()
-    self.volar_en_circulos()
-    self.volar_en_circulos()
-    self.volar_en_circulos()
+r
 
-pepita = Golondrina(100)
-anastasia = Golondrina(200)
 roberta = Dragon(10, 1000)
-maria = Golondrina(42)
 chimuelo = Dragon(200, 1000)
+hipo = Entrenador([roberta])
