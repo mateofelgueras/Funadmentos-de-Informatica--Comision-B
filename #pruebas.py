3dd10286536aba53553551df6f94bcdd505a8f08
mateofelgueras <mateofelgueras@gmail.com>
2022-06-23 19:15:01 -0300

    upd

--- #pruebas.py
+++ #pruebas.py
@@ -1,16 +1,2 @@
 #pruebas
-muestras_2 = ["d"]
-
-def obtener_media(lista):
-    sumatoria = 0
-    try:
-
-        for valor in lista:
-            sumatoria += valor
-        longitud = len(lista)
-        try:
-            return sumatoria / longitud
-        except ZeroDivisionError:
-            print("Error de división por cero")
-    except TypeError:
-        print("Solo numeros permitidos")
+import pandas as pd
